8dae7042081112f85bb18ede393da4fc08803d50
sara <sara-cg@github.com>
2017-06-29 15:31:52 +0200

    optimizing

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var keyRight1Pressed = false;
 var keyLeft1Pressed = false;
 var keyRight2Pressed = false;
 var keyLeft2Pressed = false;
-var keyJump1Pressed = false;
-var keyJump2Pressed = false;
-var keyAttack1Pressed = false;
-var keyAttack2Pressed = false;
-var keyRanged1Pressed = false;
-var keyRanged2Pressed = false;
-var keyBlock1Pressed = false;
-var keyBlock2Pressed = false;
+var keyJump1Pressed = new key();
+var keyJump2Pressed = new key();
+var keyAttack1Pressed = new key();
+var keyAttack2Pressed = new key();
+var keyRanged1Pressed = new key();
+var keyRanged2Pressed = new key();
+var keyBlock1Pressed = new key();
+var keyBlock2Pressed = new key();
 var game;
 
 var gameProperties = {
@@ -25,48 +25,147 @@ var gameProperties = {
 
 $('#start').on('click', startGame);
 
+/********************************************/
+/*******    GENERAL GAME EVENTS    **********/
+/********************************************/
+
+
 function startGame() {
-  $(".intro").css({
-    "display": "none"
-  });
-  $(".game-over").css({
-    "display": "none"
-  });
-  clearInterval(game);
-  var soundStart = document.getElementById("audioStart");
-  soundStart.play();
+
+  manageCoverScreens();
+  playStartSound();
   getGameProperties();
   playerOne = new Player(0, 0, gameProperties);
   playerTwo = new Player(0, 0, gameProperties);
-  $("#hp1color").html(playerOne.health + " HP");
-  $("#hp1color").width(playerOne.health * 3);
-  $("#hp2color").html(playerTwo.health + " HP");
-  $("#hp2color").width(playerTwo.health * 3);
-  $("#hp1color").css({
-    "background-color": "green"
-  });
-  $(".p1").css({
-    "color": "black"
-  });
-  $("#hp2color").css({
-    "background-color": "green"
-  });
-  $(".p2").css({
-    "color": "black"
-  });
-  game = setInterval(updateState, gameProperties.intervalTime);
-  var soundAmbient = document.getElementById("audioAmbient");
-  soundAmbient.play();
+  resetPlayerHealthRender();
+  setUpdateTimer();
+  startAmbientAudio();
+
 }
 
 function gameOver() {
-
   var soundGameOver = document.getElementById("audioGameOver");
   soundGameOver.play();
   clearInterval(game);
   $(".game-over").css({
     "display": "initial"
   });
+}
+
+function updateState() {
+  playerOne.verticalMovement();
+  playerTwo.verticalMovement();
+  movements();
+  meleeAtacks();
+  rangedAtacks();
+  renderLifeBars();
+  renderChara();
+}
+
+
+/********************************************/
+/*************    KEY EVENTS    *************/
+/********************************************/
+
+
+$(document).on('keydown', function(e) {
+  var soundBlock = document.getElementById("audioBlock");
+  switch (e.key) {
+    case "d":
+      keyRight1Pressed = true;
+      break;
+    case "a":
+      keyLeft1Pressed = true;
+      break;
+    case "ArrowRight":
+      keyRight2Pressed = true;
+      break;
+    case "ArrowLeft":
+      keyLeft2Pressed = true;
+      break;
+    case "w":
+      keyJump1Pressed.updateState(true);
+      break;
+    case "ArrowUp":
+      keyJump2Pressed.updateState(true);
+      break;
+    case "x":
+      keyAttack1Pressed.updateState(true);
+      break;
+    case ".":
+      keyAttack2Pressed.updateState(true);
+      break;
+    case "z":
+      keyRanged1Pressed.updateState(true);
+      break;
+    case ",":
+      keyRanged2Pressed.updateState(true);
+      break;
+    case "c":
+      keyBlock1Pressed.updateState(true);
+      soundBlock.play();
+      break;
+    case "-":
+      keyBlock2Pressed.updateState(true);
+      soundBlock.play();
+  }
+});
+
+$(document).on('keyup', function(e) {
+  switch (e.key) {
+    case "d":
+      keyRight1Pressed = false;
+      break;
+    case "a":
+      keyLeft1Pressed = false;
+      break;
+    case "ArrowRight":
+      keyRight2Pressed = false;
+      break;
+    case "ArrowLeft":
+      keyLeft2Pressed = false;
+      break;
+    case "w":
+      keyJump1Pressed.updateState(false);
+      break;
+    case "ArrowUp":
+      keyJump2Pressed.updateState(false);
+      break;
+    case "x":
+      keyAttack1Pressed.updateState(false);
+      break;
+    case ".":
+      keyAttack2Pressed.updateState(false);
+      break;
+    case "z":
+      keyRanged1Pressed.updateState(false);
+      break;
+    case ",":
+      keyRanged2Pressed.updateState(false);
+      break;
+    case "c":
+      keyBlock1Pressed.updateState(false);
+      break;
+    case "-":
+      keyBlock2Pressed.updateState(false);
+  }
+});
+
+
+/********************************************/
+/******* GAME PROPERTIES AND LOGIC **********/
+/********************************************/
+
+function getGameProperties() {
+  gameProperties.height = parseInt($("#board").css("height"));
+  gameProperties.width = parseInt($("#board").css("width"));
+  gameProperties.charaHeight = parseInt($(".chara").css("height") + $(".chara").css("margin-down"));
+  gameProperties.charaWidth = parseInt($(".chara").css("width") + $(".chara").css("margin-left") + $(".chara").css("margin-right"));
+}
+
+function setUpdateTimer() {
+  clearInterval(game);
+  game = setInterval(updateState, gameProperties.intervalTime);
 }
 
 function collision($div1, $div2) {
@@ -87,10 +186,30 @@ function collision($div1, $div2) {
   return true;
 }
 
-function updateState() {
-  playerOne.verticalMovement();
-  playerTwo.verticalMovement();
+function key() {
+  this.state = false;
+  this.justPressed = false;
+  this.updateState = function(state) {
+    if (!this.state && state) {
+      this.justPressed = true;
+    }
+    this.state = state;
+  };
+  this.wasJustPressed = function() {
+    if (this.justPressed) {
+      this.justPressed = false;
+      return true;
+    } else return false;
+  };
+}
 
+/********************************************/
+/************* CHARA ACTIONS*****************/
+/********************************************/
+
+
+
+function movements() {
   if (keyRight1Pressed) {
     if (!collision($("#chara1"), $("#chara2")))
       playerOne.movement('right');
@@ -105,13 +224,16 @@ function updateState() {
     if (!collision($("#chara1"), $("#chara2")))
       playerTwo.movement('right');
   }
-  if (keyJump1Pressed && (playerOne.y === 0)) {
+  if (keyJump1Pressed.wasJustPressed() && (playerOne.y === 0)) {
     playerOne.movement('jump');
   }
-  if (keyJump2Pressed && (playerTwo.y === 0)) {
+  if (keyJump2Pressed.wasJustPressed() && (playerTwo.y === 0)) {
     playerTwo.movement('jump');
   }
-  if (keyAttack1Pressed && (collision($("#chara1"), $("#chara2"))) && !keyBlock2Pressed && (playerTwo.y === 0)) {
+}
+
+function meleeAtacks() {
+  if (keyAttack1Pressed.wasJustPressed() && (collision($("#chara1"), $("#chara2"))) && !keyBlock2Pressed.wasJustPressed() && (playerTwo.y === 0)) {
     $("#chara2").css({
       "background-image": "url('images/chara2red.gif')"
     });
@@ -121,6 +243,22 @@ function updateState() {
       });
     }, 1000);
     playerTwo.receiveDamageMelee();
+    // $(".dmg2").css({
+    //   "display": "initial",
+    //   "opacity": 1
+    // });
+    // $(".dmg2").animate({
+    //   opacity: 0.25,
+    //   top: "+=50",
+    //   height: "toggle"
+    // }, 2000, function() {
+    //   // Animation complete.
+    // });
+    // // setTimeout(function() {
+    // //   $(".dmg2").css({
+    // //     "display": "none"
+    // //   });
+    // // }, 1000);
     var randomBackwardOne = Math.floor(Math.random() * 3) + 1;
     if (randomBackwardOne == 1) {
       playerOne.movement('left');
@@ -137,39 +275,7 @@ function updateState() {
     }
   }
 
-  if (keyRanged1Pressed && !keyBlock2Pressed && (playerTwo.y === 0)) {
-    $("#chara2").css({
-      "background-image": "url('images/chara2red.gif')"
-    });
-    setInterval(function() {
-      $("#chara2").css({
-        "background-image": "url('images/chara2.gif')"
-      });
-    }, 1000);
-    playerTwo.receiveDamageRanged();
-  }
-
-  if (playerTwo.health > 20) {
-    $("#hp2color").html(playerTwo.health + " HP");
-  } else {
-    $("#hp2color").html(playerTwo.health);
-  }
-  $("#hp2color").width(playerTwo.health * 3);
-
-  if (playerTwo.health <= 25) {
-    $("#hp2color").css({
-      "background-color": "red"
-    });
-    $(".p2").css({
-      "color": "red"
-    });
-  } else if (playerTwo.health <= 50) {
-    $("#hp2color").css({
-      "background-color": "orange"
-    });
-  }
-
-  if (keyAttack2Pressed && (collision($("#chara1"), $("#chara2"))) && !keyBlock1Pressed && (playerOne.y === 0)) {
+  if (keyAttack2Pressed.wasJustPressed() && (collision($("#chara1"), $("#chara2"))) && !keyBlock1Pressed.wasJustPressed() && (playerOne.y === 0)) {
     $("#chara1").css({
       "background-image": "url('images/chara1red.gif')"
     });
@@ -194,8 +300,22 @@ function updateState() {
       playerTwo.movement('left');
     }
   }
+}
 
-  if (keyRanged2Pressed && !keyBlock1Pressed && (playerOne.y === 0)) {
+function rangedAtacks() {
+  if (keyRanged1Pressed.wasJustPressed() && !keyBlock2Pressed.wasJustPressed() && (playerTwo.y === 0)) {
+    $("#chara2").css({
+      "background-image": "url('images/chara2red.gif')"
+    });
+    setInterval(function() {
+      $("#chara2").css({
+        "background-image": "url('images/chara2.gif')"
+      });
+    }, 1000);
+    playerTwo.receiveDamageRanged();
+  }
+
+  if (keyRanged2Pressed.wasJustPressed() && !keyBlock1Pressed.wasJustPressed() && (playerOne.y === 0)) {
     $("#chara1").css({
       "background-image": "url('images/chara1red.gif')"
     });
@@ -206,7 +326,42 @@ function updateState() {
     }, 1000);
     playerOne.receiveDamageRanged();
   }
+}
 
+/********************************************/
+/************* RENDER ***********************/
+/********************************************/
+
+
+function manageCoverScreens() {
+  $(".intro").css({
+    "display": "none"
+  });
+  $(".game-over").css({
+    "display": "none"
+  });
+}
+
+function resetPlayerHealthRender() {
+  $("#hp1color").html(playerOne.health + " HP");
+  $("#hp1color").width(playerOne.health * 3);
+  $("#hp2color").html(playerTwo.health + " HP");
+  $("#hp2color").width(playerTwo.health * 3);
+  $("#hp1color").css({
+    "background-color": "green"
+  });
+  $(".p1").css({
+    "color": "black"
+  });
+  $("#hp2color").css({
+    "background-color": "green"
+  });
+  $(".p2").css({
+    "color": "black"
+  });
+}
+
+function renderLifeBars() {
   if (playerOne.health > 20) {
     $("#hp1color").html(playerOne.health + " HP");
   } else {
@@ -227,97 +382,26 @@ function updateState() {
     });
   }
 
-  renderChara();
-}
-
-$(document).on('keydown', function(e) {
-  var soundBlock = document.getElementById("audioBlock");
-  switch (e.key) {
-    case "d":
-      keyRight1Pressed = true;
-      break;
-    case "a":
-      keyLeft1Pressed = true;
-      break;
-    case "ArrowRight":
-      keyRight2Pressed = true;
-      break;
-    case "ArrowLeft":
-      keyLeft2Pressed = true;
-      break;
-    case "w":
-      keyJump1Pressed = true;
-      break;
-    case "ArrowUp":
-      keyJump2Pressed = true;
-      break;
-    case "x":
-      keyAttack1Pressed = true;
-      break;
-    case ".":
-      keyAttack2Pressed = true;
-      break;
-    case "z":
-      keyRanged1Pressed = true;
-      break;
-    case ",":
-      keyRanged2Pressed = true;
-      break;
-    case "c":
-      keyBlock1Pressed = true;
-      soundBlock.play();
-      break;
-    case "-":
-      keyBlock2Pressed = true;
-      soundBlock.play();
+  if (playerTwo.health > 20) {
+    $("#hp2color").html(playerTwo.health + " HP");
+  } else {
+    $("#hp2color").html(playerTwo.health);
   }
-});
+  $("#hp2color").width(playerTwo.health * 3);
 
-$(document).on('keyup', function(e) {
-  switch (e.key) {
-    case "d":
-      keyRight1Pressed = false;
-      break;
-    case "a":
-      keyLeft1Pressed = false;
-      break;
-    case "ArrowRight":
-      keyRight2Pressed = false;
-      break;
-    case "ArrowLeft":
-      keyLeft2Pressed = false;
-      break;
-    case "w":
-      keyJump1Pressed = false;
-      break;
-    case "ArrowUp":
-      keyJump2Pressed = false;
-      break;
-    case "x":
-      keyAttack1Pressed = false;
-      break;
-    case ".":
-      keyAttack2Pressed = false;
-      break;
-    case "z":
-      keyRanged1Pressed = false;
-      break;
-    case ",":
-      keyRanged2Pressed = false;
-      break;
-    case "c":
-      keyBlock1Pressed = false;
-      break;
-    case "-":
-      keyBlock2Pressed = false;
+  if (playerTwo.health <= 25) {
+    $("#hp2color").css({
+      "background-color": "red"
+    });
+    $(".p2").css({
+      "color": "red"
+    });
+  } else if (playerTwo.health <= 50) {
+    $("#hp2color").css({
+      "background-color": "orange"
+    });
   }
-});
 
-function getGameProperties() {
-  gameProperties.height = parseInt($("#board").css("height"));
-  gameProperties.width = parseInt($("#board").css("width"));
-  gameProperties.charaHeight = parseInt($(".chara").css("height") + $(".chara").css("margin-down"));
-  gameProperties.charaWidth = parseInt($(".chara").css("width") + $(".chara").css("margin-left") + $(".chara").css("margin-right"));
 }
 
 function renderChara() {
@@ -325,4 +409,19 @@ function renderChara() {
   $("#chara2").css("right", playerTwo.x);
   $("#chara1").css("transform", "translateY(-" + playerOne.y + "px)");
   $("#chara2").css("transform", "translateY(-" + playerTwo.y + "px)");
+}
+
+
+/********************************************/
+/************* SOUNDS ***********************/
+/********************************************/
+
+function playStartSound() {
+  var soundStart = document.getElementById("audioStart");
+  soundStart.play();
+}
+
+function startAmbientAudio() {
+  var soundAmbient = document.getElementById("audioAmbient");
+  soundAmbient.play();
 }
